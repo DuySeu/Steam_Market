38cb4bb8cc1d22dbeb1d531d8a94a5196eee0c9d
L1ttt <longth0903@gmail.com>
2024-03-21 16:32:31 +0700

    .

---
+++ database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const fs = require('fs')
+ const connection = mysql.createConnection({ 
+	 host: 'localhost', 
+	 database: 'asignment', 
+	 user: 'root', // username of the mysql connection 
+	 password: '' // password of the mysql connection
+});
+connection.connect(function (err) {
+   if(err){
+       console.log('Error connecting' + err.stack );
+	   return;
+   }
+   else{
+       console.log('Connected as id' + connection.threadId);
+   }
+});
+
+connection.query('SELECT * FROM cases', function(error, results, fields) {
+	if(error)
+		throw error;
+		results.forEach(result => {
+			fs.writeFileSync("test.json", JSON.stringify(result));
+			
+		});
+});
+
