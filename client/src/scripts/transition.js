29d168bb3da4e20073311e20cd55e9803ca029f9
nova11781 <128200541+nova11781@users.noreply.github.com>
2024-03-26 14:53:49 +0700

    updated search case function

--- client/src/scripts/transition.js
+++ client/src/scripts/transition.js
@@ -32,30 +32,3 @@ function displayText2() {
 //   var text3 = document.getElementById('sell-an-items');
 //   text3.style.display = 'block';
 // }
-
-const casesName = document.querySelectorAll('.case-name');
-const searchInput = document.getElementById('case-search');
-
-casesName.forEach((question) => {
-  question.addEventListener('click', () => {
-    question.classList.toggle('active');
-    const answer = question.nextElementSibling;
-    answer.classList.toggle('active');
-  });
-});
-
-// JavaScript for search functionality
-searchInput.addEventListener('input', function () {
-  const searchTerm = this.value.toLowerCase();
-
-  casesName.forEach((question) => {
-    const questionText = question.textContent.toLowerCase();
-    const faqItem = question.closest('.case-display');
-
-    if (questionText.includes(searchTerm)) {
-      faqItem.style.display = 'block';
-    } else {
-      faqItem.style.display = 'none';
-    }
-  });
-});
